69602b17156fff08f96f4df4ff27b3670e46f928
t.nurislam <ntasbolatov03@gmail.com>
2024-09-01 15:31:15 +0500

    remove: mediator from sign up handler

--- src/application/auth/commands/signup.py
+++ src/application/auth/commands/signup.py
@@ -8,6 +8,7 @@ from domain.auth.value_objects.password import Password
 from application.auth.ports.jwt import JWTManager, JWTPair
 from application.auth.ports.repo import AuthRepo
 from application.common.command import Command, CommandHandler
+from application.common.uow import UnitOfWork
 
 
 @dataclass(slots=True, frozen=True)
@@ -19,20 +20,22 @@ class SignUp(Command):
 
 class SignUpHandler(CommandHandler[SignUp, JWTPair]):
     def __init__(
-        self, auth_repo: AuthRepo, mediator, uow, jwt_manager: JWTManager
+        self, auth_repo: AuthRepo, uow: UnitOfWork, jwt_manager: JWTManager
     ) -> None:
         self._auth_repo = auth_repo
         self._jwt_manager = jwt_manager
-        self._mediator = mediator
         self._uow = uow
 
     async def __call__(self, command: SignUp) -> JWTPair:
         user = entities.User(
-            password=Password(command.password),
             email=Email(command.email),
+            password=Password(command.password),
             nickname=Nickname(command.nickname),
         )
-        jwt = self._jwt_manager.create(payload=dict(sub=user.id, email=command.email))
+        jwt = self._jwt_manager.create(
+            payload=dict(sub=str(user.id), email=command.email)
+        )
         await self._auth_repo.create_user(user=user)
         await self._auth_repo.create_jwt(jwt=jwt, user_id=user.id)
+        await self._uow.commit()
         return jwt
