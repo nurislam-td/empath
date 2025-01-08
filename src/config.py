a3f09d9d8cd8b6fd99531961fcbe5d842d76539a
nurislam-td <ntasbolatov03@gmail.com>
2025-01-08 23:37:35 +0500

    add: app settings

--- src/config.py
+++ src/config.py
@@ -1,58 +1,112 @@
+import os
+from dataclasses import dataclass, field
+from functools import lru_cache
 from pathlib import Path
 
-from starlette.config import Config
+from dotenv import load_dotenv
 
-config = Config("../.env")
+load_dotenv("../.env", override=True)
 
-# App settings
-SECRET_KEY = config("SECRET_KEY", cast=str, default="ChangeIfNotDebug")
 
 BASE_DIR = Path(__file__).parent.parent
-API_V1_PREFIX = "/api/v1"
-TEMPLATE_PATH = BASE_DIR / "app" / "templates"
-MODE = config("MODE", cast=str, default="PROD")
-
-# Database settings
-DB_HOST = config("DB_HOST", cast=str, default="localhost")
-DB_USER = config("DB_USER", cast=str)
-DB_PASSWORD = config("DB_PASSWORD", cast=str)
-DB_PORT = config("DB_PORT", cast=int, default=5432)
-DB_NAME = config("DB_NAME", cast=str)
-
-ASYNC_DATABASE_URL = config(
-    "DATABASE_URL",
-    cast=str,
-    default=f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
-)
-
-SYNC_DATABASE_URL = config(
-    "SYNC_DATABASE_URL",
-    cast=str,
-    default=f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
-)
 
 
-# Auth settings
-JWT_ALG: str = "RS256"
-ACCESS_PRIVATE_PATH: Path = BASE_DIR / "certs" / "access-private.pem"
-ACCESS_PUBLIC_PATH: Path = BASE_DIR / "certs" / "access-public.pem"
-ACCESS_TOKEN_EXPIRE: int = 500000  # minutes
+@dataclass
+class DBSettings:
+    DB_HOST: str = field(
+        default_factory=lambda: os.environ.get("DB_HOST", default="localhost")
+    )
+    DB_USER: str = field(default_factory=lambda: os.environ.get("DB_USER", "db-user"))
+    DB_PASSWORD: str = field(
+        default_factory=lambda: os.environ.get("DB_PASSWORD", "db-pass")
+    )
+    DB_PORT: int = field(
+        default_factory=lambda: int(os.environ.get("DB_PORT", default=5432))
+    )
+    DB_NAME: str = field(default_factory=lambda: os.environ.get("DB_NAME", "db-name"))
 
-REFRESH_PRIVATE_PATH: Path = BASE_DIR / "certs" / "refresh-private.pem"
-REFRESH_PUBLIC_PATH: Path = BASE_DIR / "certs" / "refresh-public.pem"
-REFRESH_TOKEN_EXPIRE: int = 60 * 24 * 21  # minutes (21 days)
+    @property
+    def ASYNC_DATABASE_URL(self) -> str:
+        return os.environ.get(
+            "DATABASE_URL",
+            default=f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}",
+        )
 
-SECURE_COOKIES: bool = True
 
-VERIFICATION_CODE_EXPIRE: int = 5  # minutes
+@dataclass
+class AuthSettings:
+    JWT_ALG: str = "RS256"
+    ACCESS_PRIVATE_PATH: Path = field(
+        default_factory=lambda: Path(os.environ.get("ACCESS_PRIVATE_PATH", ""))
+    )
+    ACCESS_PUBLIC_PATH: Path = field(
+        default_factory=lambda: Path(os.environ.get("ACCESS_PUBLIC_PATH", ""))
+    )
+    ACCESS_TOKEN_EXPIRE: int = 500000  # minutes
 
-# Redis settings
-REDIS_PORT = config("REDIS_PORT", cast=int, default=6379)
-REDIS_HOST = config("REDIS_HOST", cast=str, default="localhost")
-REDIS_PREFIX = config("REDIS_PREFIX", cast=str, default="empath-cache")
-REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}"
+    REFRESH_PRIVATE_PATH: Path = field(
+        default_factory=lambda: Path(os.environ.get("REFRESH_PRIVATE_PATH", ""))
+    )
 
-# S3 settings
-S3_PRIVATE_BUCKET_NAME = config("S3_PRIVATE_BUCKET_NAME", cast=str)
-S3_PUBLIC_BUCKET_NAME = config("S3_PUBLIC_BUCKET_NAME", cast=str)
-S3_ENDPOINT_URL = config("S3_ENDPOINT_URL", cast=str)
+    REFRESH_PUBLIC_PATH: Path = field(
+        default_factory=lambda: Path(os.environ.get("ACCESS_PUBLIC_PATH", ""))
+    )
+
+    REFRESH_TOKEN_EXPIRE: int = 60 * 24 * 21  # minutes (21 days)
+
+    SECURE_COOKIES: bool = True
+
+    VERIFICATION_CODE_EXPIRE: int = 5  # minutes
+
+
+@dataclass
+class RedisSettings:
+    REDIS_PORT: int = field(
+        default_factory=lambda: int(os.environ.get("REDIS_PORT", default=6379))
+    )
+    REDIS_HOST: str = field(
+        default_factory=lambda: os.environ.get("REDIS_HOST", default="localhost")
+    )
+    REDIS_PREFIX: str = field(
+        default_factory=lambda: os.environ.get("REDIS_PREFIX", default="empath-cache")
+    )
+    REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}"
+
+
+@dataclass
+class S3Settings:
+    S3_PRIVATE_BUCKET_NAME: str = field(
+        default_factory=lambda: os.environ.get("S3_PRIVATE_BUCKET_NAME", "")
+    )
+    S3_PUBLIC_BUCKET_NAME: str = field(
+        default_factory=lambda: os.environ.get("S3_PUBLIC_BUCKET_NAME", "")
+    )
+    S3_ENDPOINT_URL: str = field(
+        default_factory=lambda: os.environ.get("S3_ENDPOINT_URL", "")
+    )
+
+
+@dataclass(frozen=True, slots=True)
+class AppSettings:
+    SECRET_KEY: str = field(
+        default_factory=lambda: os.environ.get("SECRET_KEY", "ChangeIfNotDebug")
+    )
+    API_V1_PREFIX = "/api/v1"
+    TEMPLATE_PATH = BASE_DIR / "app" / "templates"
+    ENVIRONMENT: str = field(
+        default_factory=lambda: os.environ.get("ENVIRONMENT", "PROD")
+    )
+    auth: AuthSettings = field(default_factory=AuthSettings)
+
+
+@dataclass
+class Settings:
+    app: AppSettings = field(default_factory=AppSettings)
+    db: DBSettings = field(default_factory=DBSettings)
+    s3: S3Settings = field(default_factory=S3Settings)
+    redis: RedisSettings = field(default_factory=RedisSettings)
+
+
+@lru_cache(maxsize=1, typed=True)
+def get_settings() -> Settings:
+    return Settings()
