2d42fffcada8e0374e2905c2bb8b3d4c34137f2d
nurislam-td <ntasbolatov03@gmail.com>
2025-01-08 22:24:47 +0500

    fix: refresh handler

--- src/application/auth/commands/refresh.py
+++ src/application/auth/commands/refresh.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 
 from application.auth.ports.jwt import JWTManager
+from application.auth.ports.repo import AuthRepo
 from application.common.command import Command, CommandHandler
+from application.common.uow import UnitOfWork
 from domain.auth.value_objects.jwt import JWTPair
 
 
@@ -11,9 +13,14 @@ class Refresh(Command[JWTPair]):
 
 
 class RefreshHandler(CommandHandler[Refresh, JWTPair]):
-    def __init__(self, jwt_manager: JWTManager):
+    def __init__(self, jwt_manager: JWTManager, auth_repo: AuthRepo, uow: UnitOfWork):
         self._jwt_manager = jwt_manager
+        self._auth_repo = auth_repo
+        self._uow = uow
 
     async def __call__(self, command: Refresh) -> JWTPair:
         payload = self._jwt_manager.decode_refresh(refresh_token=command.refresh_token)
-        return self._jwt_manager.create_pair(payload=payload)
+        jwt_pair = self._jwt_manager.create_pair(payload=payload)
+        await self._auth_repo.refresh_jwt(jwt=jwt_pair, user_id=payload["sub"])
+        await self._uow.commit()
+        return jwt_pair
