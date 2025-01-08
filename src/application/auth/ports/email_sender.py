151e06b129a48575a7574c7bbd5123657c353b7e
nurislam-td <ntasbolatov03@gmail.com>
2025-01-08 22:26:10 +0500

    delete: unnecessary files

--- src/application/auth/ports/email_sender.py
+++
@@ -1,5 +0,0 @@
-from typing import Protocol
-
-
-class IEmailSender(Protocol):
-    def send_email(self, message: str): ...
--- src/application/auth/ports/task_manager.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from typing import Protocol
-
-
-@dataclass
-class ITaskManager(Protocol):
-    def create_task(self, task): ...
