eeb2422ccc1aa8f08826a8099856c3d12aa55bb9
nurislam-td <ntasbolatov03@gmail.com>
2025-01-11 01:05:22 +0500

    add: verify code repo redis implementation

--- src/infrastructure/auth/repositories/verify_code.py
+++ src/infrastructure/auth/repositories/verify_code.py
@@ -1,13 +1,17 @@
 from datetime import timedelta
 
 from litestar.stores.redis import RedisStore
+from redis.asyncio import Redis
 
 from application.auth.ports.repo import VerifyCodeRepo
+from config import get_settings
+
+settings = get_settings().app.auth
 
 
 class RedisVerifyCodeRepo(VerifyCodeRepo):
-    def __init__(self, redis_client) -> None:
-        self._redis_client = RedisStore.with_client(namespace="VERIFY_CODE")
+    def __init__(self, redis_client: Redis) -> None:
+        self._redis_store = RedisStore(redis=redis_client, namespace="VERIFY_CODE")
 
     async def set_verify_code(
         self,
@@ -15,10 +19,8 @@ class RedisVerifyCodeRepo(VerifyCodeRepo):
         code: str,
         expires_in: int | timedelta | None = None,
     ):
-        expires_in = expires_in or 123
-        return await self._redis_client.set(
-            key=email, value=code, expires_in=expires_in
-        )
+        expires_in = expires_in or settings.VERIFICATION_CODE_EXPIRE
+        return await self._redis_store.set(key=email, value=code, expires_in=expires_in)
 
     async def get_verify_code(self, email: str):
-        return await self._redis_client.get(email)
+        return await self._redis_store.get(email)
--- src/infrastructure/db/config.py
+++ src/infrastructure/db/config.py
@@ -1,17 +1,25 @@
-from sqlalchemy import NullPool
+from redis.asyncio import Redis
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
-from config import ASYNC_DATABASE_URL, MODE
+from config import get_settings
 
-if MODE == "DEV":
-    DATABASE_URL = ASYNC_DATABASE_URL
-    DATABASE_PARAMS = {}
-else:
-    DATABASE_URL = ":memory:"
-    DATABASE_PARAMS = {"poolclass": NullPool}
-
+settings = get_settings()
+db_settings = settings.db
+redis_settings = settings.redis
+DATABASE_URL = db_settings.ASYNC_DATABASE_URL
+DATABASE_PARAMS = {}
 
 async_engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 async_session_maker = async_sessionmaker(
     async_engine, autoflush=False, expire_on_commit=False, autocommit=False
+)
+
+
+redis_instance = Redis.from_url(
+    url=redis_settings.URL,
+    encoding="utf-8",
+    decode_responses=False,
+    socket_connect_timeout=redis_settings.SOCKET_CONNECT_TIMEOUT,
+    socket_keepalive=redis_settings.SOCKET_KEEPALIVE,
+    health_check_interval=redis_settings.HEALTH_CHECK_INTERVAL,
 )
