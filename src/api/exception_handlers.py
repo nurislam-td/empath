701aac5a35214ce50c99e0e60bc6ee454d814b76
nurislam-td <ntasbolatov03@gmail.com>
2025-01-29 19:17:09 +0500

    [add](auth, user) : application exception handler

--- src/api/exception_handlers.py
+++ src/api/exception_handlers.py
@@ -2,18 +2,34 @@ from typing import Any
 
 from litestar import Request, Response, status_codes
 
+from application.auth.exceptions import (
+    InvalidCredentialsError,
+    InvalidPreviousPasswordError,
+    InvalidRefreshTokenError,
+    InvalidVerificationCodeError,
+)
+from application.users.exceptions import UserEmailNotExistError, UserIdNotExistError
 from domain.common.exceptions import ValueObjectError
 
 
-def value_object_error_handler(
-    _: Request[Any, Any, Any], exc: ValueObjectError
-) -> Response[dict[str, Any]]:
-    return Response(
-        status_code=status_codes.HTTP_422_UNPROCESSABLE_ENTITY,
-        content={"message": exc.message, "detail": []},
-    )
+def error_handler(status_code: int):
+    def handler(
+        _: Request[Any, Any, Any], exc: ValueObjectError
+    ) -> Response[dict[str, Any]]:
+        return Response(
+            status_code=status_code,
+            content={"message": exc.message, "detail": []},
+        )
+
+    return handler
 
 
 exception_handler = {
-    ValueObjectError: value_object_error_handler,
+    ValueObjectError: error_handler(status_codes.HTTP_422_UNPROCESSABLE_ENTITY),
+    UserIdNotExistError: error_handler(status_codes.HTTP_400_BAD_REQUEST),
+    UserEmailNotExistError: error_handler(status_codes.HTTP_400_BAD_REQUEST),
+    InvalidCredentialsError: error_handler(status_codes.HTTP_400_BAD_REQUEST),
+    InvalidPreviousPasswordError: error_handler(status_codes.HTTP_400_BAD_REQUEST),
+    InvalidRefreshTokenError: error_handler(status_codes.HTTP_400_BAD_REQUEST),
+    InvalidVerificationCodeError: error_handler(status_codes.HTTP_400_BAD_REQUEST),
 }
