b1eca9658cf60fc13e9c108693059e8871056207
nurislam-td <ntasbolatov03@gmail.com>
2025-03-03 23:05:59 +0500

    add article repo protocol

--- src/application/articles/commands/create_article.py
+++ src/application/articles/commands/create_article.py
@@ -1,23 +1,13 @@
-from dataclasses import asdict, dataclass
-from datetime import date
+from dataclasses import dataclass
 from typing import Any, TypedDict
 from uuid import UUID
 
+from application.articles.ports.repo import ArticleRepo
 from application.common.command import Command, CommandHandler
 from application.common.uow import UnitOfWork
 from domain.articles.entities import Article, SubArticle, Tag
 from domain.articles.value_objects import ArticleTitle, TagName
-
-
-class SubArticleDict(TypedDict):
-    title: str
-    text: str
-    imgs: list[str]
-
-
-class TagDict(TypedDict):
-    id: UUID
-    name: str
+from domain.common.exceptions import UnexpectedError
 
 
 @dataclass(slots=True)
@@ -26,22 +16,29 @@ class CreateArticle(Command[None]):
     title: str
     text: str
     imgs: list[str]
-    sub_articles: list[SubArticleDict]
-    tags: list[TagDict]
+    sub_articles: list[dict[str, Any]]
+    tags: list[dict[str, Any]]
     is_visible: bool
 
 
 @dataclass
 class CreateArticleHandler(CommandHandler[CreateArticle, None]):
-    _article_repo: ArticleRepo  # TODO
+    _article_repo: ArticleRepo
     _uow: UnitOfWork
 
     async def __call__(self, command: CreateArticle):
-        sub_articles = [
-            SubArticle(title=ArticleTitle(i["title"]), text=i["text"], imgs=i["imgs"])
-            for i in command.sub_articles
-        ]
-        tags = [Tag(id=i["id"], name=TagName(i["name"])) for i in command.tags]
+        try:
+            sub_articles = [
+                SubArticle(
+                    title=ArticleTitle(i["title"]), text=i["text"], imgs=i["imgs"]
+                )
+                for i in command.sub_articles
+            ]
+            tags = [Tag(id=i["id"], name=TagName(i["name"])) for i in command.tags]
+        except KeyError as e:
+            # TODO log this
+            raise UnexpectedError(f"KeyError: {e}") from e
+
         article = Article(
             title=ArticleTitle(command.title),
             text=command.text,
@@ -51,6 +48,7 @@ class CreateArticleHandler(CommandHandler[CreateArticle, None]):
             tags=tags,
             sub_articles=sub_articles,
         )
-        await self._article_repo.create_article(article)  # TODO
+
+        await self._article_repo.create_article(article)
         article.pull_events()
         await self._uow.commit()
