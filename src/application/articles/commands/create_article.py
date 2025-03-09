91985fdeec09903e8e9ff360c782e8c98fc5a77c
nurislam-td <ntasbolatov03@gmail.com>
2025-03-09 11:17:14 +0500

    add create article command

--- src/application/articles/commands/create_article.py
+++ src/application/articles/commands/create_article.py
@@ -1,24 +1,17 @@
 from dataclasses import dataclass
-from typing import Any, TypedDict
-from uuid import UUID
 
+from application.articles.dto.article import ArticleDTO
+from application.articles.mapper import (
+    convert_dto_to_article,
+)
 from application.articles.ports.repo import ArticleRepo
 from application.common.command import Command, CommandHandler
 from application.common.uow import UnitOfWork
-from domain.articles.entities import Article, SubArticle, Tag
-from domain.articles.value_objects import ArticleTitle, TagName
-from domain.common.exceptions import UnexpectedError
 
 
-@dataclass(slots=True)
-class CreateArticle(Command[None]):
-    author_id: UUID
-    title: str
-    text: str
-    imgs: list[str]
-    sub_articles: list[dict[str, Any]]
-    tags: list[dict[str, Any]]
-    is_visible: bool
+@dataclass(slots=True, frozen=True)
+class CreateArticle(Command[None], ArticleDTO):
+    pass
 
 
 @dataclass
@@ -26,29 +19,8 @@ class CreateArticleHandler(CommandHandler[CreateArticle, None]):
     _article_repo: ArticleRepo
     _uow: UnitOfWork
 
-    async def __call__(self, command: CreateArticle):
-        try:
-            sub_articles = [
-                SubArticle(
-                    title=ArticleTitle(i["title"]), text=i["text"], imgs=i["imgs"]
-                )
-                for i in command.sub_articles
-            ]
-            tags = [Tag(id=i["id"], name=TagName(i["name"])) for i in command.tags]
-        except KeyError as e:
-            # TODO log this
-            raise UnexpectedError(f"KeyError: {e}") from e
-
-        article = Article(
-            title=ArticleTitle(command.title),
-            text=command.text,
-            author_id=command.author_id,
-            imgs=command.imgs,
-            is_visible=command.is_visible,
-            tags=tags,
-            sub_articles=sub_articles,
-        )
-
-        await self._article_repo.create_article(article)
+    async def __call__(self, command: CreateArticle) -> None:
+        article = convert_dto_to_article(command)
+        await self._article_repo.create_article(command)
         article.pull_events()
         await self._uow.commit()
