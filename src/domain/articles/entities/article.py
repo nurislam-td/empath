02da24e49e1c70598a1a4e5b36bcaf0a4be2526b
nurislam-td <ntasbolatov03@gmail.com>
2025-03-12 23:21:09 +0500

    change article domain
    -remove post-init
    -add typing

--- src/domain/articles/entities/article.py
+++ src/domain/articles/entities/article.py
@@ -3,8 +3,9 @@ from uuid import UUID, uuid4
 
 from domain.articles.entities.sub_article import SubArticle
 from domain.articles.entities.tags import Tag
-from domain.articles.events import ArticleCreated, ArticleDisliked, ArticleLiked
+from domain.articles.events import ArticleDisliked, ArticleLiked
 from domain.articles.value_objects import ArticleTitle
+from domain.common.constants import Empty
 from domain.common.entities import Aggregate
 
 
@@ -22,15 +23,10 @@ class Article(Aggregate):
     dislikes_cnt: int = field(default=0)
     id: UUID = field(default_factory=uuid4)
 
-    def __post_init__(self):
-        self.record_event(ArticleCreated(author_id=self.author_id, article_id=self.id))
-
-    def like_article(self, like_owner_id: UUID):
+    def like_article(self, like_owner_id: UUID) -> None:
         self.likes_cnt += 1
         self.record_event(ArticleLiked(like_owner_id=like_owner_id, article_id=self.id))
 
-    def dislike_article(self, dislike_owner_id: UUID):
+    def dislike_article(self, dislike_owner_id: UUID) -> None:
         self.dislikes_cnt += 1
-        self.record_event(
-            ArticleDisliked(dislike_owner_id=dislike_owner_id, article_id=self.id)
-        )
+        self.record_event(ArticleDisliked(dislike_owner_id=dislike_owner_id, article_id=self.id))
