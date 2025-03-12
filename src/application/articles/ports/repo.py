a5cbdc98a3e623c3961ade9bf13058c1e13dcaaa
nurislam-td <ntasbolatov03@gmail.com>
2025-03-12 23:19:40 +0500

    article ports
    - add article reader
    - fix article repo method

--- src/application/articles/ports/repo.py
+++ src/application/articles/ports/repo.py
@@ -1,14 +1,16 @@
-from typing import Any, Protocol
+from typing import Protocol
 from uuid import UUID
 
+from application.articles.commands.create_article import CreateArticle
 from application.articles.commands.edit_article import EditArticle
-from domain.articles import entities
+from application.articles.dto.article import ArticleDTO
 
 
 class ArticleRepo(Protocol):
-    async def create_article(
-        self, article: entities.Article
-    ) -> None: ...  # TODO implement
+    async def create_article(self, article: CreateArticle) -> None: ...
+    async def update_article(self, article: EditArticle) -> None: ...
+    async def delete_article(self, article_id: UUID) -> None: ...
 
-    async def update_article(self, command: EditArticle): ...
-    async def delete_article(self, article_id: UUID): ...
+
+class ArticleReader(Protocol):
+    async def get_article_by_id(self, article_id: UUID) -> ArticleDTO: ...
