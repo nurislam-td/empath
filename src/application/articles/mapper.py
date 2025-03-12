f2805f8988c08708d5a87ac06d23ba2fb5e54e9e
nurislam-td <ntasbolatov03@gmail.com>
2025-03-12 23:20:02 +0500

    add article application mapper

--- src/application/articles/mapper.py
+++ src/application/articles/mapper.py
@@ -1,38 +1,40 @@
+from collections.abc import Callable
+from typing import Any
+
 from application.articles.dto.article import ArticleDTO, SubArticleDTO, TagDTO
 from domain.articles.entities import SubArticle
 from domain.articles.entities.article import Article
 from domain.articles.entities.tags import Tag
 from domain.articles.value_objects import ArticleTitle
 from domain.articles.value_objects.tag_name import TagName
+from domain.common.constants import Empty
 
 
-def convert_dto_to_subarticle(dto: SubArticleDTO):
-    return SubArticle(
-        text=dto.text, title=ArticleTitle(dto.title), imgs=dto.imgs, id=dto.id
-    )
+def convert_dto_to_subarticle(dto: SubArticleDTO) -> SubArticle:
+    return SubArticle(text=dto.text, title=ArticleTitle(dto.title), imgs=dto.imgs, id=dto.id)
 
 
-def convert_dto_to_tag(dto: TagDTO):
+def convert_dto_to_tag(dto: TagDTO) -> Tag:
     return Tag(name=TagName(dto.name), id=dto.id)
 
 
-def convert_dto_to_article(dto: ArticleDTO):
+def convert_dto_to_article(dto: ArticleDTO) -> Article:
     tags = [convert_dto_to_tag(i) for i in dto.tags]
     sub_articles = (
-        [convert_dto_to_subarticle(i) for i in dto.sub_articles]
-        if dto.sub_articles
-        else []
+        [convert_dto_to_subarticle(i) for i in dto.sub_articles] if dto.sub_articles is not Empty.UNSET else []
     )
+    dto_dict = dto.to_dict(exclude_unset=True)
     return Article(
+        **dto_dict,
+        author_id=dto.author.id,
         title=ArticleTitle(dto.title),
-        text=dto.text,
-        author_id=dto.author_id,
         tags=tags,
-        is_visible=dto.is_visible,
-        imgs=dto.imgs,
         sub_articles=sub_articles,
-        views_cnt=dto.views_cnt,
-        likes_cnt=dto.likes_cnt,
-        dislikes_cnt=dto.dislikes_cnt,
-        id=dto.id,
     )
+
+
+convert_strategy: dict[str, Callable[[Any], Any]] = {
+    "title": lambda value: ArticleTitle(value),
+    "sub_articles": lambda value: [convert_dto_to_subarticle(dto) for dto in value],
+    "tags": lambda value: [convert_dto_to_tag(dto) for dto in value],
+}
