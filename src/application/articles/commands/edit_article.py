1b0b117e9c493030ad48e149aae87a400a586a57
nurislam-td <ntasbolatov03@gmail.com>
2025-03-12 23:18:27 +0500

    add article command
    - create article
    - edit article

--- src/application/articles/commands/edit_article.py
+++ src/application/articles/commands/edit_article.py
@@ -1,28 +1,44 @@
 from dataclasses import dataclass
-from typing import Any
 from uuid import UUID
 
-from application.articles.ports.repo import ArticleRepo
+from application.articles.dto.article import SubArticleDTO, TagDTO
+from application.articles.exceptions import EmptyArticleUpdatesError
+from application.articles.mapper import (
+    convert_dto_to_article,
+    convert_strategy,
+)
+from application.articles.ports.repo import ArticleReader, ArticleRepo
 from application.common.command import Command, CommandHandler
 from application.common.uow import UnitOfWork
+from domain.common.constants import Empty
 
 
-@dataclass
+@dataclass(frozen=True, slots=True)
 class EditArticle(Command[None]):
-    title: str
-    text: str
+    id: UUID
     author_id: UUID
-    tags: list[str]
-    is_visible: bool
-    imgs: list[str]
-    sub_articles: list[dict[str, Any]]
+    title: str | Empty = Empty.UNSET
+    text: str | Empty = Empty.UNSET
+    tags: list[TagDTO] | Empty = Empty.UNSET
+    is_visible: bool | Empty = Empty.UNSET
+    imgs: list[str] | Empty = Empty.UNSET
+    sub_articles: list[SubArticleDTO] | Empty = Empty.UNSET
 
 
 @dataclass
-class UpdateAvatarHandler(CommandHandler[EditArticle, None]):
+class UpdateArticleHandler(CommandHandler[EditArticle, None]):
     _article_repo: ArticleRepo
+    _article_reader: ArticleReader
     _uow: UnitOfWork
 
     async def __call__(self, command: EditArticle) -> None:
+        article_dto = await self._article_reader.get_article_by_id(command.id)
+        article = convert_dto_to_article(article_dto)
+        if len(keys := command.to_dict_exclude_unset().keys()) == 2 and {"author_id", "id"} <= keys:
+            raise EmptyArticleUpdatesError()
+        for attr, value in command.to_dict().items():
+            convert_fun = convert_strategy.get(attr, lambda x: x)
+            setattr(article, attr, convert_fun(value))
+
         await self._article_repo.update_article(command)
         await self._uow.commit()
