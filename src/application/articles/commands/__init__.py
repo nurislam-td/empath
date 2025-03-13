62ede831792da9f6785aac56a22fc67f20b0fb40
nurislam-td <ntasbolatov03@gmail.com>
2025-03-13 22:37:38 +0500

    fix article circular import

---
+++ src/application/articles/commands/__init__.py
@@ -0,0 +1,9 @@
+from .create_article import CreateArticle
+from .delete_article import DeleteArticle
+from .edit_article import EditArticle
+
+__all__ = (
+    "CreateArticle",
+    "DeleteArticle",
+    "EditArticle",
+)
--- src/application/articles/ports/repo.py
+++ src/application/articles/ports/repo.py
@@ -1,14 +1,15 @@
-from typing import Protocol
+from typing import TYPE_CHECKING, Protocol
 from uuid import UUID
 
-from application.articles.commands.create_article import CreateArticle
-from application.articles.commands.edit_article import EditArticle
 from application.articles.dto.article import ArticleDTO
+
+if TYPE_CHECKING:
+    from application.articles.commands import CreateArticle, EditArticle
 
 
 class ArticleRepo(Protocol):
-    async def create_article(self, article: CreateArticle) -> None: ...
-    async def update_article(self, article: EditArticle) -> None: ...
+    async def create_article(self, article: "CreateArticle") -> None: ...
+    async def update_article(self, article: "EditArticle") -> None: ...
     async def delete_article(self, article_id: UUID) -> None: ...
 
 
