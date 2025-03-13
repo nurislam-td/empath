794fc35ef2b10bfabb021267214c76c42646ce83
nurislam-td <ntasbolatov03@gmail.com>
2025-03-13 22:38:26 +0500

    rewrite base model to new alchemy syntax

--- src/infrastructure/db/models/base.py
+++ src/infrastructure/db/models/base.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from uuid import UUID
 
 import sqlalchemy as sa
-from sqlalchemy.orm import DeclarativeBase, registry
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, registry
 
 convention = {
     "ix": "ix_%(column_0_label)s",  # INDEX
@@ -21,7 +22,13 @@ class BaseModel(DeclarativeBase):
     metadata = mapper_registry.metadata
 
     __abstract__ = True
-    id: sa.Column[UUID] = sa.Column(
+    # id: sa.Column[UUID] = sa.Column(
+    #     sa.types.Uuid,
+    #     primary_key=True,
+    #     server_default=sa.text("gen_random_uuid()"),
+    #     index=True,
+    # )
+    id: Mapped[UUID] = mapped_column(
         sa.types.Uuid,
         primary_key=True,
         server_default=sa.text("gen_random_uuid()"),
@@ -33,11 +40,7 @@ class TimedBaseModel(BaseModel):
     """An abstract base model that adds created_at and updated_at timestamp fields to the model."""
 
     __abstract__ = True
-
-    created_at = sa.Column(sa.types.DateTime, nullable=False, server_default=sa.func.now())
-    updated_at = sa.Column(
-        sa.types.DateTime,
-        nullable=False,
-        server_default=sa.func.now(),
-        onupdate=sa.func.now(),
+    created_at: Mapped[datetime] = mapped_column(sa.types.DateTime, nullable=False, server_default=sa.func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        sa.types.DateTime, nullable=False, server_default=sa.func.now(), onupdate=sa.func.now()
     )
