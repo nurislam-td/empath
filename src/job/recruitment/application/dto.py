782d926e912aa60ea4afb2fa8bb980e72be02475
nurislam-td <ntasbolatov03@gmail.com>
2025-04-08 00:22:13 +0500

    fix vacancy detail

--- src/job/recruitment/application/dto.py
+++ src/job/recruitment/application/dto.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from uuid import UUID
 
 from common.application.dto import DTO
-from job.common.application.dto import SalaryDTO
+from job.common.application.dto import EmploymentTypeDTO, SalaryDTO, SkillDTO, WorkFormatDTO, WorkScheduleDTO
 from job.common.domain.enums import EducationEnum, WorkExpEnum, WorkFormatEnum
 
 
@@ -42,18 +42,17 @@ class DetailedVacancyDTO(DTO):
     salary: SalaryDTO
     author: DetailedAuthorDTO
     work_exp: WorkExpEnum
-    work_schedules: list[str]
-    employment_types: list[str]
-    work_formats: list[str]
-    skills: list[str]
-    additional_skills: list[str]
+    work_schedules: list[WorkScheduleDTO]
+    employment_types: list[EmploymentTypeDTO]
+    work_formats: list[WorkFormatDTO]
+    skills: list[SkillDTO]
+    additional_skills: list[SkillDTO]
     created_at: datetime
     email: str
-    id: UUID
     is_visible: bool
-    work_format: WorkFormatEnum
     responsibility: str
     requirements: str
     education: EducationEnum
     additional_description: str | None
     address: str | None
+    id: UUID
