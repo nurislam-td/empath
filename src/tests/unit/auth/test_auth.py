d087c521e90d32983067da530f2a07c2d2c87c12
nurislam-td <ntasbolatov03@gmail.com>
2025-05-11 00:33:29 +0500

    add signup validators test

--- src/tests/unit/auth/test_auth.py
+++ src/tests/unit/auth/test_auth.py
@@ -1,4 +1,4 @@
-from typing import Unpack
+from typing import Unpack, cast
 
 import pytest
 from dishka import AsyncContainer
@@ -7,6 +7,9 @@ from mimesis import Field, Generic, Locale, Schema
 from auth.application.commands.signup import SignUp, SignUpHandler
 from auth.application.ports.repo import AuthReader
 from users.application.ports.repo import UserReader
+from users.domain.value_objects.email import MismatchEmailError
+from users.domain.value_objects.nickname import TooLongNicknameError
+from users.domain.value_objects.password import MismatchPasswordError
 
 from .protocols import UserData, UserDataFactory, UserDataKwargs
 
@@ -24,7 +27,7 @@ def user_data_factory(generic_generator: Generic) -> UserDataFactory:
             },
             iterations=1,
         )
-        return UserData(**schema.create()[0], **kwargs)
+        return cast("UserData", {**schema.create()[0]} | {**kwargs})
 
     return factory
 
@@ -34,13 +37,17 @@ def user_data(user_data_factory: UserDataFactory) -> UserData:
     return user_data_factory()
 
 
+@pytest.fixture
+async def signup_handler(request_container: AsyncContainer) -> SignUpHandler:
+    return await request_container.get(SignUpHandler)
+
+
 async def test_signup(
     request_container: AsyncContainer,
     user_data: UserData,
+    signup_handler: SignUpHandler,
 ) -> None:
-    handler = await request_container.get(SignUpHandler)
-
-    await handler(
+    await signup_handler(
         SignUp(
             email=user_data["email"],
             password=user_data["password"],
@@ -53,3 +60,29 @@ async def test_signup(
     user = await user_repo.get_user_by_email(user_data["email"])
     assert user
     assert await token_repo.get_refresh_token(user.id)
+
+
+@pytest.mark.parametrize(
+    ("fields", "exc"),
+    [
+        ({"email": "invalid_email"}, MismatchEmailError),
+        ({"password": "short"}, MismatchPasswordError),
+        ({"nickname": "a" * 52}, TooLongNicknameError),
+    ],
+)
+async def test_signup_validations(
+    fields: UserDataKwargs,
+    exc: type[Exception],
+    user_data_factory: UserDataFactory,
+    signup_handler: SignUpHandler,
+) -> None:
+    user_data = user_data_factory(**fields)
+
+    with pytest.raises(exc):
+        await signup_handler(
+            SignUp(
+                email=user_data["email"],
+                password=user_data["password"],
+                nickname=user_data["nickname"],
+            ),
+        )
